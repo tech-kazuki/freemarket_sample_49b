cda10108b01c9d1c8ecc8f414b07ae3f74658e80
hamasakiharuya <hamasakiharuya@MacBook-Pro.local>
2019-04-25 18:39:46 +0900

    choice registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
- devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+ devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root "products#index"
+
+ devise_scope :user do
+  get 'index' => 'users/registrations#index'
+end
 
 end
