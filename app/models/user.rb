c7ce56143ac4697debfc92b36a513b4251f1db03
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-25 16:47:37 +0900

    add_validates_katakana

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
-  validates :family_name_katakana, presence: true
-  validates :first_name_katakana, presence: true
+  validates :family_name_katakana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ }
+  validates :first_name_katakana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ }
   validates :birthday_year, presence: true
   validates :birthday_month, presence: true
   validates :birthday_date, presence: true
