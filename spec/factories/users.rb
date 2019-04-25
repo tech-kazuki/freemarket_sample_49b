52b671f654a2cdff28ab39ec6dddfd3a360c8578
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-25 15:27:02 +0900

    Add faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
-    family_name           {"山田"}
-    first_name            {"彩"}
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.email}
+    password              {Faker::Number.number(6)}
+    password_confirmation {Faker::Number.number(6)}
+    family_name           {Faker::Name.last_name}
+    first_name            {Faker::Name.first_name}
     family_name_katakana  {"ヤマダ"}
     first_name_katakana   {"アヤ"}
-    birthday_year         {"1990"}
-    birthday_month        {"3"}
-    birthday_date         {"3"}
+    birthday_year         {Faker::Number.number(4)}
+    birthday_month        {Faker::Number.number}
+    birthday_date         {Faker::Number.number}
   end
 
 end
