08878c32526cf943e248fca93bd0643d2130f5e1
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-25 14:41:11 +0900

    Add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_katakana, presence: true
+  validates :first_name_katakana, presence: true
+  validates :birthday_year, presence: true
+  validates :birthday_month, presence: true
+  validates :birthday_date, presence: true
 end
