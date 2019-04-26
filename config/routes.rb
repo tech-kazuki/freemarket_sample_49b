3140d6ddc14ff7bd2db689d70bc175acd59d4451
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-26 12:31:39 +0900

    Add_views_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
- devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   root "products#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:show] do
+    resource :address, only: [:new, :create, :edit, :update, :destroy]
+    resource :card, only: [:new, :create, :edit, :update, :destroy]
+  end
   devise_scope :user do
     get 'index' => 'users/registrations#index'
   end
