c660fe52a8c5d01e1c7601291d96cf2b198c45c4
hamasakiharuya <hamasakiharuya@MacBook-Pro.local>
2019-04-27 20:12:53 +0900

    fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -27,23 +27,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
-  def facebook
-    callback_from :facebook
-  end
-
-  private
-
-  def callback_from(provider)
-    provider = provider.to_s
-
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth'].merge(password: @user.encrypted_password)
-      redirect_to new_user_registration_path
-    end
-  end
+  
+  
 end
