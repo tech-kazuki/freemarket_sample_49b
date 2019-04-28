05e83371ecdcab05e9ec2b13a37c831b606ebd2b
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-28 14:54:38 +0900

    edit_files

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   }
   
   root "products#index"
-  get 'products/buy' => 'products#buy'
-  resources :products, only: [:index, :new]
+  resources :products, only: [:index, :new] do
+    collection do
+      get 'buy'
+    end
+  end
   resources :users, only: [:show] do
     resource :address, only: [:new, :create, :edit, :update, :destroy]
     resource :card, only: [:new, :create, :edit, :update, :destroy]
