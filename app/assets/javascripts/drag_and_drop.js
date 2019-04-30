4d7a22375e7ef2c0e700027b7d7a41a6c59dd5a7
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-30 12:38:45 +0900

    4/30 morning

--- app/assets/javascripts/drag_and_drop.js
+++ app/assets/javascripts/drag_and_drop.js
@@ -1,23 +1,65 @@
 $(document).on('turbolinks:load', function() {
-  let dropBox = document.getElementById('drop_box');
+  var dropBox = document.getElementById('drop_box');
+  var input = document.getElementById('file_input');
+  var textBox = document.getElementById('text_field');
   
   if (dropBox) {
+    
+    function organizeFiles(files) {
+      var length = files.length;
+      var file;
+      for (var i = 0; i < length; i++) {
+        file = files[i];
+          
+        outputImage(file);
+      }
+    }
+    
+    function outputImage(file) {
+      var blobURL = URL.createObjectURL(file);
+      $('.drop_area').prepend(`<div class="drop_area__image_box">
+                                <div class="drop_content">
+                                  <img src="${blobURL}" class="drop_content__img" style="">
+                                </div>
+                              <a href="" class="delete_link">削除</a>
+                              </div>`);
+    }
+    
+    function getImage(imageURL) {
+      $('.drop_area').prepend(`<div class="drop_area__image_box">
+                              <div class="drop_content">
+                                <img src="${imageURL}" class="drop_content__img" style="">
+                              </div>
+                            <a href="" class="delete_link">削除</a>
+                            </div>`);
+    }
+
     dropBox.addEventListener('dragover', function(e) {
       e.preventDefault();
-      
+
       e.dataTransfer.dropEffect = 'copy';
-    });
+    }, false);
     
     dropBox.addEventListener('dragleave', function () {
-    });
+    }, false);
     
     dropBox.addEventListener('drop', function (e) {
       e.preventDefault();
-      let imageData = e.dataTransfer.getData('copy');
-      $('.drop_area').prepend(`<div class="drop_area__image_box">
-                                <div class="drop_img" style=""></div>
-                                <a href="" class="delete_link">削除</a>
-                                </div>`);
+      
+      organizeFiles(e.dataTransfer.files);
+    }, false);
+    
+    $('#file_input').on('change', function(e){
+      e.preventDefault();
+      var imgs = e.target.files;
+      for (var j = 0; j < imgs.length; j++) {
+        textBox.value = imgs[j];
+        var imgURL = URL.createObjectURL(imgs[j]);
+        getImage(imgURL);
+      }
+    });
+    
+    $('.products_new_container__content__title').on('click', function(){
     });
     
     $('#drop_box').on('click', function () {
