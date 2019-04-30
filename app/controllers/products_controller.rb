77326e557435233e0af696b439bf5d59a251eb87
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-30 18:31:04 +0900

    Add_save_images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,22 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+    @product.images.build
+  end
+  
+  def create
+    product = Product.new(product_params)
+    product.user = current_user
+    product.save
   end
 
   def buy
+  end
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :category_id, :product_state, :burden, :size, :prefecture_id, :how_long, :how_ship, :brand_id, :availability, images_attributes: [:image])
   end
 end
