cb648220695332f8cbdfdb8f9681f40059701904
hamasakiharuya <hamasakiharuya@MacBook-Pro.local>
2019-05-04 17:56:34 +0900

    category-page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,16 @@ class CategoriesController < ApplicationController
   def index
   end
 
+  def show
+    @category= Category.find(params[:id])
+    categoryIds = Category.find(params[:id]).indirect_ids
+    if categoryIds.empty?
+      categoryIds = Category.find(params[:id]).children
+      if categoryIds.empty?
+        categoryIds = Category.find(params[:id])
+      end
+    end
+    @products = Product.where(category_id: categoryIds)
+  end
+
 end
