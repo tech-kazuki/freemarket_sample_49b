11f0160d36cb82bfe7a89c8b480f477cc37f9247
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-05-06 14:55:21 +0900

    add_test_deletefile_

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user, optional: true
   belongs_to_active_hash :prefecture
+  
+  validates :postal_code,   presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: '例）123-1234' }
+  validates :city,          presence: true
+  validates :prefecture_id, presence: true
 end
