78eeb043d7b306de3989f8d162682f706c8693af
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-05-06 17:03:21 +0900

    validates_edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,8 +27,8 @@ class ProductsController < ApplicationController
       respond_to do |format|
         format.json
       end
-     end
-   
+    end
+
     @product = Product.new
     @user = User.find(current_user.id)
     @product.images.build
