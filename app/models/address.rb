e1e0466e3550db6df54079d8d820273d832d9b6d
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-05-06 14:59:10 +0900

    add_address_validates

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,11 @@ class Address < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to_active_hash :prefecture
   
-  validates :postal_code,   presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: '例）123-1234' }
-  validates :city,          presence: true
-  validates :prefecture_id, presence: true
+  validates :family_name,           presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/ , message: "全角で入力してください" }
+  validates :first_name,            presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/ , message: "全角で入力してください" }
+  validates :family_name_katakana,  presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ , message: "全角カタカナで入力してください" }
+  validates :first_name_katakana,   presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ , message: "全角カタカナで入力してください" }
+  validates :postal_code,           presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: '例）123-1234' }
+  validates :city,                  presence: true
+  validates :prefecture_id,         presence: true
 end
