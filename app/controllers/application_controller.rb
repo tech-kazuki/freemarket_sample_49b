2497356750932ad4ca758088d6955a3d038ab6ca
hamasakiharuya <hamasakiharuya@MacBook-Pro.local>
2019-05-06 16:51:07 +0900

    add breadcrumbs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
   before_action :call_category
+  before_action :request_path
   protect_from_forgery with: :exception
 
   private
-
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
       keys: [:nickname, :family_name, :first_name, :family_name_katakana, :first_name_katakana, :birthday_year, :birthday_month, :birthday_date, :image, :profile, :provider, :uid,
@@ -19,6 +20,13 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
+  def request_path
+    @path = controller_path + '#' + action_name
+    def @path.is(*str)
+        str.map{|s| self.include?(s)}.include?(true)
     end
   end
 
