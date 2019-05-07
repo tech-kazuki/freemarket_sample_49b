4c5a52cf34751ab4ebe2578707fdf4fcd0f5b159
sei1916 <pinodas1916@gmail.com>
2019-05-07 15:13:02 +0900

    [WIP] 商品一覧表示
    # WHAT
    商品一覧表示実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,13 @@
 class ProductsController < ApplicationController
   def index
+    @category1 = Category.find_by(id: 15)
+    categoryLadys = @category1.indirects
+    @Ladys = Product.all.where(category_id: categoryLadys).order('created_at DESC').limit(4)
+
+    @category2 = Category.find_by(id: 22)
+    categoryMens = @category2.indirects
+    @Mens = Product.all.where(category_id: categoryMens).order('created_at DESC').limit(4)
+
   end
 
   def show
