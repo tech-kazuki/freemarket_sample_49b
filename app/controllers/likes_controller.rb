e14b41f4528fc96a8a9f2962bbff3d1b1c40af89
hamasakiharuya <hamasakiharuya@MacBook-Pro.local>
2019-05-07 14:06:05 +0900

    add method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,25 @@
 class LikesController < ApplicationController
 
   def create
-    @like = Like.create(user_id: current_user.id, product_id: params[:product_id])
-    @likes = Like.where(product_id: params[:product_id])
+    @like = Like.create(like_params)
+    @likes = Like.where(get_product_id)
     @product = Product.find(params[:product_id])
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, product_id: params[:product_id])
+    @like = Like.find_by(like_params)
     @like.destroy
-    @likes = Like.where(product_id: params[:product_id])
+    @likes = Like.where(get_product_id)
     @product = Product.find(params[:product_id])
+  end
+
+  private
+  def like_params
+    params.permit(:product_id).merge(user_id: current_user.id)
+  end
+
+  def get_product_id
+   params.permit(:product_id)
   end
 
 end
