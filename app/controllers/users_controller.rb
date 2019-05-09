9bc35fdd63608d23c980febaf4eb52827a17f4f4
sei1916 <pinodas1916@gmail.com>
2019-05-09 19:09:58 +0900

    変数修正
    変数修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @products = Product.count(params[:id])
+    @products_count = Product.count(params[:id])
   end
 
   def edit
