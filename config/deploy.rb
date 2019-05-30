3e05d7c1a9f602db01308fccb96c3f482f46d915
tech-kazuki <kazness11@gmail.com>
2019-05-30 18:56:40 +0900

    add_seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,4 +28,14 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+  desc 'db_seed'
+  task :db_seed do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
 end
